2d42da76d84f6c010f6a60f2032e647ca5d2f563
xiaoluTang <2935347599@qq.com>
2018-09-06 23:41:39 +0800

    title(需求):小窗口播放器相关

--- app/src/main/java/com/example/txl/tool/player/SmallAndFullScreenChangePlayerActivity.java
+++ app/src/main/java/com/example/txl/tool/player/SmallAndFullScreenChangePlayerActivity.java
@@ -7,7 +7,11 @@ import android.support.constraint.ConstraintLayout;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 import com.example.txl.tool.BaseActivity;
 import com.example.txl.tool.R;
@@ -23,26 +27,70 @@ public class SmallAndFullScreenChangePlayerActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_small_and_full_screen_change_player );
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);//display = getWindowManager().getDefaultDisplay();display.getMetrics(dm)（把屏幕尺寸信息赋值给DisplayMetrics dm）;
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        Log.d( TAG,"缩放比例   "+getResources().getDisplayMetrics().density +"  "+dm.density+"  "+width+"  height:"+height);
+
+        FrameLayout frameLayout = new FrameLayout( this ){
+            @Override
+            public void addView(View child) {
+                super.addView( child );
+            }
+        };
+        frameLayout.setLayoutParams( new ViewGroup.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT ) );
+        setContentView( frameLayout);
+        new PlayerUiController<FrameLayout>( frameLayout,this );
     }
 
 
 
-    public class PlayerUiController<T extends ViewGroup>{
-        T parent;
+    /**
+     * 持有播放器相应的事件回调接口，对应事件进行处理；播放控制器;
+     * 参照IMediaPlayer接口，设计播放器相关接口,初步设想应该持有一个播放控制器
+     * */
+    public class PlayerUiController<T> implements View.OnClickListener{
+        ViewGroup parent;
         ConstraintLayout rootConstraintLayout;
-        public PlayerUiController(T parent, Context context) {
+        ImageView ivBack, ivMore, ivPlayerController, ivChangeToFull;
+        SeekBar playerSeekBar;
+
+        T playerController;
+
+        public PlayerUiController(ViewGroup parent, Context context) {
             this.parent = parent;
-            rootConstraintLayout = (ConstraintLayout) LayoutInflater.from( context ).inflate( R.layout.activity_small_and_full_screen_change_player,parent,false );
+            initView(context);
         }
 
-        private void initView(){
+        private void initView(Context context){
 
+            rootConstraintLayout = (ConstraintLayout) LayoutInflater.from( context ).inflate( R.layout.activity_small_and_full_screen_change_player,parent,false );
+            parent.addView(rootConstraintLayout);
+            ivBack = rootConstraintLayout.findViewById( R.id.iv_small_player_ui_back );
+            ivBack.setOnClickListener( this );
+            ivMore = rootConstraintLayout.findViewById( R.id.iv_small_player_ui_more );
+            ivMore.setOnClickListener( this );
+            ivPlayerController = rootConstraintLayout.findViewById( R.id.iv_small_player_ui_play_controller );
+            ivPlayerController.setOnClickListener( this );
+            ivChangeToFull = rootConstraintLayout.findViewById( R.id.iv_small_player_ui_change_to_full );
+            ivChangeToFull.setOnClickListener( this );
+            playerSeekBar = rootConstraintLayout.findViewById( R.id.sb_small_player_ui_seek );
+            parent.invalidate();
+            // TODO: 2018/9/6 playerSeekBar 添加对应的事件
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.iv_small_player_ui_back:
+                    break;
+                case R.id.iv_small_player_ui_more:
+                    break;
+                case R.id.iv_small_player_ui_play_controller:
+                    break;
+                case R.id.iv_small_player_ui_change_to_full:
+                    break;
+            }
+        }
+
+        public void setPlayerController(T playerController){
+            this.playerController = playerController;
         }
     }
 }
