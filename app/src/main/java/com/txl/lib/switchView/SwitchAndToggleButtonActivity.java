d8b93e728d44ab9970c512b7b8e824eb1d04cb5b
xiaoluTang <2935347599@qq.com>
2018-11-19 22:58:30 +0800

    title(需求):开关按钮

---
+++ app/src/main/java/com/txl/lib/switchView/SwitchAndToggleButtonActivity.java
@@ -0,0 +1,15 @@
+package com.txl.lib.switchView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.txl.tool.R;
+
+public class SwitchAndToggleButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_switch_and_toggle_button );
+    }
+}
