fece41e041989193e88c7168d3aaffe4ac077527
xiaolutang <2935347599@qq.com>
2018-12-21 18:34:25 +0800

    title:日历（不全）

---
+++ app/src/main/java/com/txl/lib/calendar/CalendarActivity.java
@@ -0,0 +1,15 @@
+package com.txl.lib.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.txl.tool.R;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
