fb2fcfc5d8e6f7b8659073329377d87ef009baff
xiaolutang <2935347599@qq.com>
2019-01-24 21:58:40 +0800

    title:嵌套滑动研究

--- app/src/main/java/com/example/txl/tool/NestedScrolling/LinearNestedScrollParent.java
+++ app/src/main/java/com/example/txl/tool/NestedScrolling/LinearNestedScrollParent.java
@@ -2,8 +2,10 @@ package com.example.txl.tool.NestedScrolling;
 
 import android.content.Context;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.NestedScrollingParent2;
 import android.support.v4.view.NestedScrollingParentHelper;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,7 +15,7 @@ import android.widget.LinearLayout;
  * @author TXL
  * description :嵌套滑动demo
  */
-public class LinearNestedScrollParent extends LinearLayout {
+public class LinearNestedScrollParent extends LinearLayout implements NestedScrollingParent2 {
     NestedScrollingParentHelper nestedScrollingParentHelper;
 
     public LinearNestedScrollParent(Context context) {
@@ -33,16 +35,40 @@ public class LinearNestedScrollParent extends LinearLayout {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
+    {
+        nestedScrollingParentHelper = new NestedScrollingParentHelper(this);
+    }
+
     @Override
-    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
         return true;
     }
 
     @Override
-    public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+        nestedScrollingParentHelper.onNestedScrollAccepted(child,target,axes,type);
+    }
 
-        consumed[0] = dx;
-        consumed[1] = dy;
-        scrollTo(dx,dy);
+    @Override
+    public void onStopNestedScroll(@NonNull View target, int type) {
+        nestedScrollingParentHelper.onStopNestedScroll(target,type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @Nullable int[] consumed, int type) {
+        int d = 0;
+        if(dy>50){
+            d = consumed[1] = 50;
+
+        }else {
+            d = consumed[1] = dy;
+        }
+        scrollBy(0,d);
+
     }
 }
