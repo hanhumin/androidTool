3e05c4a8ffdd5ee76e48963c3e3a00f529cc0abf
xiaolutang <2935347599@qq.com>
2019-04-11 09:40:01 +0800

    title:测试在不切换播放源的情况下，切换TextureView查看播放情况

--- app/src/main/java/com/example/txl/tool/gank/io/video/MoreTextureActivity.java
+++ app/src/main/java/com/example/txl/tool/gank/io/video/MoreTextureActivity.java
@@ -1,5 +1,7 @@
 package com.example.txl.tool.gank.io.video;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -8,11 +10,14 @@ import android.os.Bundle;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.txl.tool.R;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 测试多个TextureView同时使用的情况
@@ -34,32 +39,6 @@ public class MoreTextureActivity extends AppCompatActivity {
             public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
 
                 mediaPlayer.setSurface( new Surface( surface ) );
-                mediaPlayer.setOnPreparedListener( new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-                        mp.start();
-                    }
-                } );
-                mediaPlayer.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
-                    @Override
-                    public void onCompletion(MediaPlayer mp) {
-                        mediaPlayer.setSurface( new Surface( textureView2.getSurfaceTexture() ) );
-
-//                        try {
-//                            mediaPlayer.reset();
-//                            mediaPlayer.setDataSource( MoreTextureActivity.this, Uri.parse("https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200ff70000bdcrer2k781i9vhhqsvg&line=0") );
-//                            mediaPlayer.prepareAsync();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-                    }
-                } );
-                try {
-                    mediaPlayer.setDataSource( MoreTextureActivity.this, Uri.parse("https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200ff50000bd0p9ur6936mllnbeo40&line=0") );
-                    mediaPlayer.prepareAsync();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
 
             @Override
@@ -77,14 +56,43 @@ public class MoreTextureActivity extends AppCompatActivity {
 
             }
         } );
+        try {
+            mediaPlayer.setOnPreparedListener( new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            } );
+            mediaPlayer.setOnCompletionListener( new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.setSurface( new Surface( textureView2.getSurfaceTexture() ) );
+
+//                        try {
+//                            mediaPlayer.reset();
+//                            mediaPlayer.setDataSource( MoreTextureActivity.this, Uri.parse("https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200ff70000bdcrer2k781i9vhhqsvg&line=0") );
+//                            mediaPlayer.prepareAsync();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+                }
+            } );
+            mediaPlayer.setDataSource( MoreTextureActivity.this, Uri.parse("https://aweme.snssdk.com/aweme/v1/playwm/?video_id=v0200ff50000bd0p9ur6936mllnbeo40&line=0") );
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         textureView2 = findViewById( R.id.more_texture_activity_TextureView2 );
         TextView tvPlay1 = findViewById(R.id.tv_play_1);
         tvPlay1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.setSurface( new Surface( textureView1.getSurfaceTexture() ) );
-                textureView1.setVisibility(View.VISIBLE);
-                textureView2.setVisibility(View.INVISIBLE);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);//横屏
+                ViewGroup viewGroup = (ViewGroup) textureView1.getParent();
+                viewGroup.removeAllViews();
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                textureView1.setLayoutParams(params);
+                viewGroup.addView(textureView1);
             }
         });
         TextView tvPlay2 = findViewById(R.id.tv_play_2);
