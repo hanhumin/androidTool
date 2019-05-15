6955678e0f29555d16875cb65021015542329b70
xiaolutang <2935347599@qq.com>
2019-05-15 18:41:47 +0800

    title(需求):intent

---
+++ app/src/main/java/com/example/txl/tool/intent/Intent1Activity.kt
@@ -0,0 +1,15 @@
+package com.example.txl.tool.intent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.txl.tool.R
+/**
+ * 从
+ * */
+class Intent1Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent1)
+    }
+}
---
+++ app/src/main/java/com/example/txl/tool/intent/Intent2Activity.kt
@@ -0,0 +1,16 @@
+package com.example.txl.tool.intent
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import com.example.txl.tool.R
+
+class Intent2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent2)
+    }
+}
