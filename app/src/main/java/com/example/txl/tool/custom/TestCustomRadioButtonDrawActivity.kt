2d9ddaa270c5c4ee45305ed5174ca4c0e2ea5f0c
xiaolutang <2935347599@qq.com>
2020-02-28 13:09:33 +0800

    title(需求):radio button 绘制

--- app/src/main/java/com/example/txl/tool/custom/TestCustomRadioButtonDrawActivity.kt
+++ app/src/main/java/com/example/txl/tool/custom/TestCustomRadioButtonDrawActivity.kt
@@ -1,13 +1,27 @@
 package com.example.txl.tool.custom
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.txl.tool.R
+import kotlinx.android.synthetic.main.activity_test_custom_radio_button_draw.*
 
 class TestCustomRadioButtonDrawActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_custom_radio_button_draw)
+    }
+
+    var change = false
+
+    fun changeColor(view: View) {
+        if(change){
+            test.setCircleTitleBgColor(Color.WHITE)
+        }else{
+            test.setCircleTitleBgColor(Color.RED)
+        }
+        change = !change
     }
 }
--- app/src/main/java/com/example/txl/tool/custom/TestRadioButtonDraw.java
+++ app/src/main/java/com/example/txl/tool/custom/TestRadioButtonDraw.java
@@ -1,12 +1,24 @@
 package com.example.txl.tool.custom;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.widget.RadioButton;
+
+import androidx.annotation.ColorInt;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.example.txl.tool.R;
 
@@ -35,7 +47,7 @@ public class TestRadioButtonDraw extends RadioButton {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        drawCircleTitleBg(canvas,0);
+        drawCircleTitleBg(canvas,Color.RED);
         super.onDraw(canvas);
     }
 
@@ -43,21 +55,140 @@ public class TestRadioButtonDraw extends RadioButton {
      * 绘制一个顶部圆弧的背景
      * */
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private BitmapDrawable circleTitleBgDrawable;
+    private int circleTitleBgColor = Color.WHITE;
     private void drawCircleTitleBg(Canvas canvas,  int color){
+        if(circleTitleBgDrawable == null){
+            setCircleTitleBgColor(circleTitleBgColor);
+            return;
+        }
         paint.setAntiAlias(true);
-        paint.setColor(Color.RED);
+        paint.setColor(color);
         paint.setStyle(Paint.Style.FILL);
-        paint.setStrokeWidth(getResources().getDimensionPixelOffset(R.dimen.dime_50dp));
+
         int height = getHeight();
         int width = getWidth();
-        int topHeight = getResources().getDimensionPixelOffset(R.dimen.dime_24dp);
-        canvas.drawRect(0,topHeight,width,height,paint);
-        Path mPath = new Path();
-        mPath.moveTo(15, topHeight);
-        mPath.quadTo(width/2.0f, 0, width-15, topHeight);
-        canvas.drawPath(mPath, paint);
-//        canvas.drawLine(0,0,width,height,paint);
-//        canvas.drawCircle(width/2.0f,height/2.0f,topHeight,paint);
-//        canvas.drawColor(Color.RED);
+
+        float topHeight = getResources().getDimensionPixelSize(R.dimen.dimen24);
+        paint.setStrokeWidth(topHeight);
+        final int scrollX = getScrollX();
+        final int scrollY = getScrollY();
+        float r = ((width-15) * (width-15) / (8f * topHeight)) +  (topHeight / 2f);
+        if (scrollX == 0 && scrollY == 0) {
+//            circleTitleBgDrawable.draw(canvas);
+            circleTitleBgDrawable = (BitmapDrawable) ContextCompat.getDrawable(getContext(),R.drawable.top_circle_bg);
+            Drawable wrappedDrawable = DrawableCompat.wrap(circleTitleBgDrawable);
+            wrappedDrawable = wrappedDrawable.mutate();
+            DrawableCompat.setTint(wrappedDrawable, Color.RED);
+            wrappedDrawable.setBounds(new Rect(0,0,width, (int) topHeight));
+            circleTitleBgDrawable.setGravity(Gravity.CENTER_HORIZONTAL + Gravity.TOP);
+            circleTitleBgDrawable.draw(canvas);
+//            canvas.drawBitmap(circleTitleBgDrawable.getBitmap(),new Matrix(),paint);
+//            canvas.drawBitmap(circleTitleBgDrawable.getBitmap(),null,new Rect(0,0,width, (int) topHeight),paint);
+        } else {
+            canvas.translate(scrollX, scrollY);
+//            canvas.drawBitmap(circleTitleBgDrawable.getBitmap(),new Matrix(),paint);
+//            circleTitleBgDrawable.draw(canvas);
+            canvas.translate(-scrollX, -scrollY);
+        }
     }
+
+    public void setCircleTitleBgColor(@ColorInt int titleBgColor){
+        circleTitleBgColor = titleBgColor;
+        circleTitleBgDrawable = circleTitleBgDrawable(titleBgColor);
+        invalidate();
+    }
+
+    private BitmapDrawable circleTitleBgDrawable(@ColorInt int titleBgColor){
+        Drawable drawable = ContextCompat.getDrawable(getContext(),R.drawable.top_circle_bg);
+        if(drawable == null || getWidth() == 0){
+            return null;
+        }
+        final Drawable wrappedDrawable = DrawableCompat.wrap(drawable);
+        wrappedDrawable.mutate();
+        DrawableCompat.setTint(wrappedDrawable, titleBgColor);
+        drawable = wrappedDrawable;
+        //目标图片的宽高
+        int targetWidth=getWidth() > drawable.getIntrinsicWidth()? drawable.getIntrinsicWidth():getWidth();
+        int targetHeight= drawable.getIntrinsicHeight();
+        //宽高应该是当前button的宽高
+        Bitmap bitmap=Bitmap.createBitmap(targetWidth, targetHeight, Bitmap.Config.ARGB_8888);
+        BitmapDrawable bgDrawable = new BitmapDrawable(bitmap);
+        Canvas canvas = new Canvas(bitmap);
+        int color=Color.TRANSPARENT;
+        canvas.drawColor(color, PorterDuff.Mode.ADD);
+        Paint paint=new Paint();
+        paint.setColor(color);
+        paint.setFilterBitmap(true);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(new Rect(0, 0, targetWidth, targetHeight), paint);
+        Bitmap defaultLoading=((BitmapDrawable)drawable).getBitmap();
+        defaultLoading = scaleImage(defaultLoading,targetWidth,targetHeight);//先将图片缩放到希望的大小
+        Matrix matrix=new Matrix();
+        float translateX=0.0f,translateY=0.0f;
+        if(defaultLoading.getHeight()>targetHeight||defaultLoading.getWidth()>targetWidth)
+        {
+            float scaleX=1.0f,scaleY=1.0f;
+            float scaleW,scaleH;
+            if(defaultLoading.getHeight()>targetHeight&&defaultLoading.getWidth()>targetWidth)
+            {
+                scaleY=1.0f*targetHeight/defaultLoading.getHeight();
+                scaleX=scaleY;
+            }
+            else if(defaultLoading.getHeight()>targetHeight)
+            {
+                scaleY=1.0f*targetWidth/defaultLoading.getHeight();
+                scaleX=scaleY;
+            }
+            else if(defaultLoading.getWidth()>targetWidth)
+            {
+                scaleX=1.0f*targetWidth/defaultLoading.getWidth();
+                scaleY=scaleX;
+            }
+            scaleW=defaultLoading.getWidth()*scaleX;
+            scaleH=defaultLoading.getHeight()*scaleY;
+            translateX=(targetWidth-scaleW)/2.0f;
+            translateY=(targetHeight-scaleH)/2.0f;
+            matrix.postScale(scaleX, scaleY);
+        }
+        else
+        {
+            translateX=(targetWidth-defaultLoading.getWidth())/2.0f;
+            translateY=(targetHeight-defaultLoading.getHeight())/2.0f;
+        }
+        matrix.postTranslate(translateX, translateY);
+        paint.setColor(Color.RED);
+        canvas.drawBitmap(defaultLoading, matrix, paint);
+        bgDrawable.draw(canvas);
+        return bgDrawable;
+    }
+
+    /**
+     * 缩放图片
+     * @param bm 要缩放图片
+     * @param newWidth 宽度
+     * @param newHeight 高度
+     * @return处理后的图片
+     */
+    public Bitmap  scaleImage(Bitmap bm, float newWidth, float newHeight){
+        if (bm == null){
+            return null;
+        }
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth =  newWidth / width;
+        float scaleHeight =  newHeight / height;
+        if(scaleWidth == scaleHeight && scaleWidth == 1){
+            return bm;
+        }
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, width, height, matrix,true);
+        if (bm != null & !bm.isRecycled()){
+//            bm.recycle();//销毁原图片
+            bm = null;
+        }
+        return newbm;
+    }
+
 }
