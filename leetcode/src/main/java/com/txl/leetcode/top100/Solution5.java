6e2a1a603e1159d2bf0f914c8765e4b7b25b7c2e
tangxiaolu <2935347599@qq.com>
2021-06-01 17:03:11 +0800

    title: 5 回文子串

--- leetcode/src/main/java/com/txl/leetcode/top100/Solution5.java
+++ leetcode/src/main/java/com/txl/leetcode/top100/Solution5.java
@@ -45,9 +45,12 @@ package com.txl.leetcode.top100;
 // 👍 3683 👎 0
 
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution5 {
     public static void main(String[] args){
+        System.out.println(new Solution5().solution3("babad"));
         System.out.println(new Solution5().longestPalindrome("babad"));
     }
 
@@ -88,5 +91,124 @@ class Solution5 {
         }
         return s.charAt(0)+"";
     }
+
+    public String solution2(String s){
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+        // 递推开始
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
+                int j = L + i - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+
+                // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    public String solution3(String s){
+        if(s == null || s.length() == 1){
+            return s;
+        }
+        int maxLen = 1;
+        HashMap<PositionKey,Boolean> positionInfo1 = new HashMap<>();
+        HashMap<PositionKey,Boolean> positionInfo2 = new HashMap<>();
+        HashMap<PositionKey,Boolean> temp = null;
+        int begin = 0;
+        int len = s.length();
+        for (int L = 2; L <= len; L++) {
+            for (int i=0;i<=len-L;i++){
+                int j = L + i -1;
+                boolean value = s.charAt(i) == s.charAt(j);
+                if(L == 2){
+                    positionInfo1.put(new PositionKey(i,j), value);
+                }else if(L == 3){
+                    positionInfo2.put(new PositionKey(i,j), value);
+                }else {
+                    if(temp == null){
+                        temp = new HashMap<>();
+                    }
+                    temp.put(new PositionKey(i,j), value && positionInfo1.get(new PositionKey(i+1,j-1)));
+                }
+                if(value && L > maxLen){//是回文
+                    if(L == 2 || L == 3){
+                        begin = i;
+                        maxLen = L;
+                    }else if(positionInfo1.get(new PositionKey(i+1,j-1))){
+                        begin = i;
+                        maxLen = L;
+                    }
+                }
+
+            }
+            if(temp != null){
+                positionInfo1 = positionInfo2;
+                positionInfo2 = temp;
+                temp = null;
+            }
+
+        }
+        return s.substring(begin,begin+maxLen);
+    }
+
+    private static class PositionKey{
+        private final int i;
+        private final int j;
+
+        public PositionKey(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj instanceof  PositionKey){
+                return ((PositionKey) obj).i == this.i && ((PositionKey) obj).j == this.j;
+            }
+            return false;
+        }
+
+
+        @Override
+        public int hashCode() {//每一轮循环中的i值都是不相同的，可以直接用i 做hash
+            return Integer.hashCode(i);
+        }
+    }
+
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
