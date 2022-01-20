f161e20d346689f0dd9679a753d3f2b8206f5448
tangxiaolu <2935347599@qq.com>
2022-01-20 18:35:42 +0800

    title:leetcode 212

---
+++ leetcode/src/main/java/com/txl/leetcode/code/Solution212.java
@@ -0,0 +1,70 @@
+package com.txl.leetcode.code;
+
+import java.util.List;
+
+public class Solution212 {
+    public List<String> findWords(char[][] board, String[] words) {
+        WordDictionary wordDictionary = new WordDictionary();
+        for (String word : words) {
+            wordDictionary.addWord(word);
+        }
+
+    }
+
+    class WordDictionary {
+        //是否为结束标记
+        boolean isEnd = false;
+        WordDictionary[] wordDictionaries = new WordDictionary[26];
+//        WordDictionary root;
+
+        public WordDictionary() {
+
+        }
+
+        public void addWord(String word) {
+            WordDictionary root = this;
+            for (int i = 0; i < word.length(); i++) {
+                //没必要考虑这个输入的时候有. 题目已经明确输入只有小写字母
+//                char c = word.charAt(i) == '.' ? 'a' : word.charAt(i);
+                int index = word.charAt(i) - 'a';
+                if(root.wordDictionaries[index] == null){
+                    root.wordDictionaries[index] = new WordDictionary();
+                }
+                root = root.wordDictionaries[index];
+            }
+            root.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            return search(this,word,0);
+        }
+
+        private boolean search(WordDictionary wordDictionary, String word, int index){
+            if(wordDictionary == null){
+                return false;
+            }
+            WordDictionary root = wordDictionary;
+            for (int i = index; i < word.length(); i++) {
+                //遇到. 表示当前节点下的所有可用子节点都可以遍历
+                //如果搜索的最后一个字符是 . 在进入下一级循环的时候 因为index = word.length() 不能进入 循环，判断依据是root.isEnd  逻辑ok
+                //如果搜索的字符包含 .
+                if(word.charAt(i) == '.'){
+                    for (int j = 0; j < 26; j++) {
+                        if(search(root.wordDictionaries[j],word,i+1)){
+                            return true;
+                        }
+                    }
+                    return false;
+                }else {
+                    int p = word.charAt(i) - 'a';
+                    if(root.wordDictionaries[p] != null){
+                        root = root.wordDictionaries[p];
+                    }else {//没有找到直接返回false
+                        return false;
+                    }
+                }
+            }
+            return root!= null && root.isEnd;
+        }
+    }
+}
