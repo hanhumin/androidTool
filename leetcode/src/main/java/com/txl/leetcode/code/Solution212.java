e5486c4bb87d6e5d94928fe51b4115614914207b
tangxiaolu <2935347599@qq.com>
2022-01-21 18:22:51 +0800

    title:leetcode 212

--- leetcode/src/main/java/com/txl/leetcode/code/Solution212.java
+++ leetcode/src/main/java/com/txl/leetcode/code/Solution212.java
@@ -1,19 +1,59 @@
 package com.txl.leetcode.code;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Solution212 {
+
+    public static void main(String[] args){
+        //fixme 为什么是空数组？
+        System.out.println("result is : "+new Solution212().findWords(new char[][]{{'a'}},new String[]{"a"}));
+    }
+
+    int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
     public List<String> findWords(char[][] board, String[] words) {
         WordDictionary wordDictionary = new WordDictionary();
         for (String word : words) {
             wordDictionary.addWord(word);
         }
+        HashSet<String> result = new HashSet<>();
+        for (int i=0; i< board.length; i++){
+            for (int j = 0; j < board[0].length;j++){
+                dfs(board,wordDictionary,result,i,j);
+            }
+        }
+        return new ArrayList<>(result);
+    }
 
+    private void dfs(char[][] board, WordDictionary wordDictionary, HashSet<String> result, int i0, int j0){
+        if(!"".equals(wordDictionary.word)){//查找到了以此结尾的单词
+            result.add(wordDictionary.word);
+        }
+        //i0 j0 是网格中将要处理的位置
+        char ch = board[i0][j0];
+        if(ch == '#'){//当前位置访问过，退出
+            return;
+        }
+        int index = ch - 'a';
+        if(wordDictionary.wordDictionaries[index] == null){//这个字符在当前树中不存在
+            return;
+        }
+        board[i0][j0] = '#';
+        for (int i=0;i<dirs.length;i++){
+            int ii = dirs[i][0] + i0;
+            int jj = dirs[i][1] + j0;
+            if(ii < board.length && ii >= 0 && jj < board[0].length && jj >= 0){
+                dfs(board,wordDictionary.wordDictionaries[index],result,ii,jj);
+            }
+        }
+        board[i0][j0] = ch;
     }
 
     class WordDictionary {
         //是否为结束标记
-        boolean isEnd = false;
+        String word = "";
         WordDictionary[] wordDictionaries = new WordDictionary[26];
 //        WordDictionary root;
 
@@ -32,39 +72,8 @@ public class Solution212 {
                 }
                 root = root.wordDictionaries[index];
             }
-            root.isEnd = true;
-        }
-
-        public boolean search(String word) {
-            return search(this,word,0);
-        }
-
-        private boolean search(WordDictionary wordDictionary, String word, int index){
-            if(wordDictionary == null){
-                return false;
-            }
-            WordDictionary root = wordDictionary;
-            for (int i = index; i < word.length(); i++) {
-                //遇到. 表示当前节点下的所有可用子节点都可以遍历
-                //如果搜索的最后一个字符是 . 在进入下一级循环的时候 因为index = word.length() 不能进入 循环，判断依据是root.isEnd  逻辑ok
-                //如果搜索的字符包含 .
-                if(word.charAt(i) == '.'){
-                    for (int j = 0; j < 26; j++) {
-                        if(search(root.wordDictionaries[j],word,i+1)){
-                            return true;
-                        }
-                    }
-                    return false;
-                }else {
-                    int p = word.charAt(i) - 'a';
-                    if(root.wordDictionaries[p] != null){
-                        root = root.wordDictionaries[p];
-                    }else {//没有找到直接返回false
-                        return false;
-                    }
-                }
-            }
-            return root!= null && root.isEnd;
+            //记录插入的字符
+            root.word = word;
         }
     }
 }
